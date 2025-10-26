49ab30452e146704dab4a15419dfb71c11c9b25a
HafeezSalim <59242442+HafeezSalim@users.noreply.github.com>
2025-10-26 11:59:45 +0800

    populate firstNum set up

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const calculatorState = {
     
-    firstNum: "5",
-    operator: "+",
-    secondNum: "3",
+    firstNum: "",
+    operator: "",
+    secondNum: "",
 
     add: function() {
         let result = parseInt(this.firstNum) + parseInt(this.secondNum);
@@ -45,8 +45,89 @@ const calculatorState = {
         return result;
     },
 
+    populateFirstNum: function () {
+        switch(event.target.id) {
+            case "ZeroButton":
+                if (this.firstNum=="0") {
+                    //do nothing
+                } else {
+                    this.firstNum = this.firstNum + "0";
+                }
+                break;
+            case "OneButton":
+                if (this.firstNum=="0") {
+                    this.firstNum = "1";
+                } else {
+                    this.firstNum = this.firstNum + "1";
+                }
+                break;
+            case "TwoButton":
+                if (this.firstNum=="0") {
+                    this.firstNum = "2";
+                } else {
+                    this.firstNum = this.firstNum + "2";
+                }
+                break;
+            case "ThreeButton":
+                if (this.firstNum=="0") {
+                    this.firstNum = "3";
+                } else {
+                    this.firstNum = this.firstNum + "3";
+                }
+                break;
+            case "FourButton":
+                if (this.firstNum=="0") {
+                    this.firstNum = "4";
+                } else {
+                    this.firstNum = this.firstNum + "4";
+                }
+                break;
+            case "FiveButton":
+                if (this.firstNum=="0") {
+                    this.firstNum = "5";
+                } else {
+                    this.firstNum = this.firstNum + "5";
+                }
+                break;
+            case "SixButton":
+                if (this.firstNum=="0") {
+                    this.firstNum = "6";
+                } else {
+                    this.firstNum = this.firstNum + "6";
+                }
+                break;
+            case "SevenButton":
+                if (this.firstNum=="0") {
+                    this.firstNum = "7";
+                } else {
+                    this.firstNum = this.firstNum + "7";
+                }
+                break;
+            case "EightButton":
+                if (this.firstNum=="0") {
+                    this.firstNum = "8";
+                } else {
+                    this.firstNum = this.firstNum + "8";
+                }
+                break;
+            case "NineButton":
+                 if (this.firstNum=="0") {
+                    this.firstNum = "9";
+                } else {
+                    this.firstNum = this.firstNum + "9";
+                }
+                break;   
+        }
+    },
+
     updateDisplay: function(event) {
-        console.log(event.target.id + " was clicked.");
+        let display1 = document.querySelector(".Line1");
+        display1.textContent="";
+        if (event.target.classList.contains("Number") && this.operator=="" && this.secondNum=="") {
+            this.populateFirstNum();
+        }
+        display1.textContent=this.firstNum;
+        
     }
 
 };
